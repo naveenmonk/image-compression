7b391dae4e856de7a473738c328f40c2ff0c117d
NaveenMONK <naveenbalaji786@gmail.com>
2022-03-11 15:26:49 +0530

    Added files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import ImageCompressor from './components/image-compressor';
+
+function App() {
+  return (
+    <div className="App">
+      <ImageCompressor />
+    </div>
+  );
+}
+
+export default App;
